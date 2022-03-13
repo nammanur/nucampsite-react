d612a82c01d86e0b55999e638d35e1a8e884a6b4
Narayanan Ammanur <nammanur@gmail.com>
2022-03-13 08:36:09 +0530

    React Router.

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,8 +1,8 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return (
-            <Card onClick={()=> onClick(campsite.id)}>
+            <Card>
                     <CardImg src={campsite.image} width="100%" src={campsite.image} alt={campsite.name}/>
                     <CardImgOverlay>
                         <CardTitle>{campsite.name}</CardTitle>
@@ -16,7 +16,7 @@ function Directory(props) {
     const directory = props.campsites.map((campsite)=>{
         return (
         <div className='col-md-5 m-1' key={campsite.id} >
-            <RenderDirectoryItem campsite={campsite} onClick={props.onClick}/>
+            <RenderDirectoryItem campsite={campsite}/>
         </div>)
     });
     return (<div className='container'>
@@ -26,28 +26,6 @@ function Directory(props) {
     </div>);
 }
 
-// class ExampleParentComponent extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             number: 333
-//         };
-//     }
-
-//     render() {
-//         return <ExampleChildComponent num={this.state.number} greeting ="hello world"></ExampleChildComponent>
-//     }
-
-// }
-
-// class ExampleChildComponent extends Component {
-
-
-//     render() {
-//         return <div>{this.props.num} {this.props.greeting}</div>
-//     }
-
-// }
 
 
 export default Directory;   
